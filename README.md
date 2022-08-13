18366393bc1dfed0bd7593424b878992009c9b91
JRhin <pandolfo_mario@yahoo.com>
2022-08-13 12:17:21 +0200

    changes

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 A repository containing my personal functions library.
 
-## visualization.py
+## `visualization.py`
 
 Some useful functions used for visualization purposes.
 
-* **plot_decision_boundary()**:
+* `plot_decision_boundary()`:
   Plots decision boundaries of model predicting on X in comparison to y.
   - Input:
-    - model: a `torch.nn.Module`
-    - X: a `torch.Tensor`
-    - Y: a `torch.Tensor`
+    - model (`torch.nn.Module`): our model.
+    - X (`torch.Tensor`): The data on which we predict.
+    - Y (`torch.Tensor`): The true labels.
   - Ouput:
     - None
--- visualization.py
+++ visualization.py
@@ -6,12 +6,13 @@ def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Ten
     """Plots decision boundaries of model predicting on X in comparison to y.
 
     Input:
-        model: torch.nn.Module
-        X: torch.Tensor
-        y: torch.Tensor
+        model (torch.nn.Module): Our model.
+        X (torch.Tensor): The data on which we predict.
+        y (torch.Tensor): The true labels.
 
     Output:
         None
+
     Source - https://madewithml.com/courses/foundations/neural-networks/ (with modifications)
     """
     # Put everything to CPU (works better with NumPy + Matplotlib)
